6c1d0d53df8b4f9b559f1611fbf3792a1cde6a1d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-23 12:54:02 -0500

    reformat: Enforce linting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { controlCarouselTransitions } from "./modules/carouselController"
-import "./styles/styles.css"
+import { controlCarouselTransitions } from "./modules/carouselController";
+import "./styles/styles.css";
 
 /**When the next or prev button is clicked,
  * pick the next or prev sibling to the current image.
- * 
+ *
  * If current image is the first or last element,
  * then pick the first or last element in the node list
  * of images. (Wrap-around)
--- src/modules/carouselController.js
+++ src/modules/carouselController.js
@@ -1,14 +1,14 @@
-const CAROUSEL_CONTAINER = document.querySelector('.carousel-outer');
-const CAROUSEL = document.querySelector('.carousel');
-const CAROUSEL_INDICATOR = document.querySelector('.carousel-indicator');
+const CAROUSEL_CONTAINER = document.querySelector(".carousel-outer");
+const CAROUSEL = document.querySelector(".carousel");
+const CAROUSEL_INDICATOR = document.querySelector(".carousel-indicator");
 
-const IMAGES_ARRAY = [...CAROUSEL.querySelectorAll('.carousel-img-container')];
-const INDICATOR_ARRAY = [...CAROUSEL_INDICATOR.querySelectorAll('li')];
+const IMAGES_ARRAY = [...CAROUSEL.querySelectorAll(".carousel-img-container")];
+const INDICATOR_ARRAY = [...CAROUSEL_INDICATOR.querySelectorAll("li")];
 let CAROUSEL_INDEX = 0;
 
 export function controlCarouselTransitions() {
-    initializeArrayIndex();
-    initializeCarouselListeners();
+  initializeArrayIndex();
+  initializeCarouselListeners();
 }
 
 /**
@@ -16,69 +16,69 @@ export function controlCarouselTransitions() {
  * which child indicator element has the active class.
  */
 function initializeArrayIndex() {
-    CAROUSEL_INDEX = INDICATOR_ARRAY.findIndex((element) => {
-        return element.classList.contains('active');
-    });
+  CAROUSEL_INDEX = INDICATOR_ARRAY.findIndex((element) => {
+    return element.classList.contains("active");
+  });
 }
 
 function initializeCarouselListeners() {
-    CAROUSEL_CONTAINER.addEventListener('click', (event) => {
-        if (event.target instanceof HTMLButtonElement) {
-            const clickedButton = event.target;
-            handleCarouselControlClick(clickedButton);
-        }
+  CAROUSEL_CONTAINER.addEventListener("click", (event) => {
+    if (event.target instanceof HTMLButtonElement) {
+      const clickedButton = event.target;
+      handleCarouselControlClick(clickedButton);
+    }
 
-        if (event.target instanceof HTMLLIElement) {
-            const slideTo = event.target.dataset.slideTo;
-            handleIndicatorClick(slideTo);
-        }
-    });
+    if (event.target instanceof HTMLLIElement) {
+      const slideTo = event.target.dataset.slideTo;
+      handleIndicatorClick(slideTo);
+    }
+  });
 }
 
 function handleIndicatorClick(slideTo) {
-    removeCurrentActiveClassElements();
-    CAROUSEL_INDEX = (Number(slideTo) - 1);
-    setActiveElements();
+  removeCurrentActiveClassElements();
+  CAROUSEL_INDEX = Number(slideTo) - 1;
+  setActiveElements();
 }
 
 function handleCarouselControlClick(clickedButton) {
-    const direction = clickedButton.dataset.direction;
+  const direction = clickedButton.dataset.direction;
 
-    if (direction === 'next') {
-        removeCurrentActiveClassElements();
-        incrementCarouselIndex();
-        setActiveElements();
-    }
+  if (direction === "next") {
+    removeCurrentActiveClassElements();
+    incrementCarouselIndex();
+    setActiveElements();
+  }
 
-    if (direction === 'previous') {
-        removeCurrentActiveClassElements();
-        decrementCarouselIndex();
-        setActiveElements();
-    }
+  if (direction === "previous") {
+    removeCurrentActiveClassElements();
+    decrementCarouselIndex();
+    setActiveElements();
+  }
 }
 
 function removeCurrentActiveClassElements() {
-    IMAGES_ARRAY[CAROUSEL_INDEX].classList.remove('active');
-    INDICATOR_ARRAY[CAROUSEL_INDEX].classList.remove('active');
+  IMAGES_ARRAY[CAROUSEL_INDEX].classList.remove("active");
+  INDICATOR_ARRAY[CAROUSEL_INDEX].classList.remove("active");
 }
 
 function setActiveElements() {
-    IMAGES_ARRAY[CAROUSEL_INDEX].classList.add('active');
-    INDICATOR_ARRAY[CAROUSEL_INDEX].classList.add('active');
+  IMAGES_ARRAY[CAROUSEL_INDEX].classList.add("active");
+  INDICATOR_ARRAY[CAROUSEL_INDEX].classList.add("active");
 }
 
 function incrementCarouselIndex() {
-    if (CAROUSEL_INDEX !== (INDICATOR_ARRAY.length - 1)) {
-        CAROUSEL_INDEX += 1;
-    } else {
-        CAROUSEL_INDEX = 0;
-    }
+  if (CAROUSEL_INDEX !== INDICATOR_ARRAY.length - 1) {
+    CAROUSEL_INDEX += 1;
+  } else {
+    CAROUSEL_INDEX = 0;
+  }
 }
 
 function decrementCarouselIndex() {
-    if (CAROUSEL_INDEX > 0) {
-        CAROUSEL_INDEX -= 1;
-    } else {
-        CAROUSEL_INDEX = (INDICATOR_ARRAY.length - 1);
-    }
+  if (CAROUSEL_INDEX > 0) {
+    CAROUSEL_INDEX -= 1;
+  } else {
+    CAROUSEL_INDEX = INDICATOR_ARRAY.length - 1;
+  }
 }
